535aa273ed46f6ce07b7e9226fec2f0322040e56
jhon1119D <cuper96@hotmail.com>
2024-12-28 15:29:59 +0100

    Tu mensaje de commit aquíi

--- Router.php
+++ Router.php
@@ -20,8 +20,8 @@ class Router
     public function comprobarRutas()
     {
 
-        $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
-        // $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
+        // $currentUrl = $_SERVER['PATH_INFO'] ?? '/';
+        $currentUrl = strtok($_SERVER['REQUEST_URI'], '?') ?? '/';
         
         $method = $_SERVER['REQUEST_METHOD'];
 
